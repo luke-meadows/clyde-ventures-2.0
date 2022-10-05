13a7265936416e33f9591bcb64f3662a0f7bb8b7
luke-meadows <lukemeadowsdev@gmail.com>
2022-10-05 11:59:08 +0100

    new analytics

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,15 @@ function MyApp({ Component, pageProps }) {
   return (
     <Page>
       <SEO {...nextSeoConfig} />
-
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-T43JJP9');
+      `}
+      </Script>
       <Header setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
       <AnimatePresence initial={false}>
         {showSidebar && (
