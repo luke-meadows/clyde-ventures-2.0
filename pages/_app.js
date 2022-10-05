7e3f7e529f5f52d78a5cb0b3bf8e381e13a27b52
luke-meadows <lukemeadowsdev@gmail.com>
2022-10-05 11:56:02 +0100

    new analytics

--- pages/_app.js
+++ pages/_app.js
@@ -17,14 +17,6 @@ function MyApp({ Component, pageProps }) {
     <Page>
       <SEO {...nextSeoConfig} />
 
-      <Script strategy="lazyOnload" id="data-layer">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-T43JJP9');`}
-      </Script>
-
       <Header setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
       <AnimatePresence initial={false}>
         {showSidebar && (
