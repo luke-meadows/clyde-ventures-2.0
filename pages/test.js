778cc20f9df35b99cd1c65f73368802adbd44a41
luke-meadows <lukemeadowsdev@gmail.com>
2022-10-05 14:57:47 +0100

    button click event test

--- pages/test.js
+++ pages/test.js
@@ -1,11 +1,16 @@
 import styled from 'styled-components';
-export default function test() {
+export default function Test() {
   return (
     <div style={{ paddingTop: '15rem' }}>
       <button
-        onClick={() => dataLayer.push({ submitted_contact_form: 'submitted' })}
+        onClick={() => dataLayer.push({ event: 'submitted_contact_form' })}
       >
-        Button 1
+        Button 5
+      </button>
+      <button
+        onClick={() => dataLayer.push({ event: 'submitted_contact_form' })}
+      >
+        Button 5
       </button>
     </div>
   );
