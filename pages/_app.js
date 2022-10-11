da1db907d2be35b117f5e14c9e08266f1a1cb465
luke-meadows <lukemeadowsdev@gmail.com>
2022-10-11 12:51:56 +0100

    script ids

--- pages/_app.js
+++ pages/_app.js
@@ -17,11 +17,12 @@ function MyApp({ Component, pageProps }) {
     <Page>
       <SEO {...nextSeoConfig} />
       <Script
+        id="googleAnalytics"
         strategy="lazyOnload"
         src={`https://www.googletagmanager.com/gtag/js?id=G-FYR84LJVTR`}
       />
 
-      <Script strategy="lazyOnload">
+      <Script strategy="lazyOnload" id="googleAnalyticsDataLayer">
         {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
