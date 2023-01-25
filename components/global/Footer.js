bc8c30a87fe4dc430def8ddb8091b226103f7142
luke-meadows <lukemeadowsdev@gmail.com>
2023-01-25 15:22:20 +0000

    modern-slavery-statement

--- components/global/Footer.js
+++ components/global/Footer.js
@@ -49,9 +49,7 @@ export default function Footer() {
             <Link href="/privacy-policy" alt="Privacy Policy">
               Privacy Policy
             </Link>
-            <Link href="/modern-slavery-statement">
-              Modern Slavery Statement
-            </Link>
+            <Link href="/modern-slavery">Modern Slavery Statement</Link>
             {/* <Link href="/">Governance Policies</Link> */}
           </nav>
           <p className="copy">
