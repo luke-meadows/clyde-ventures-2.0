d9b1a91b6c7ccdd3c24a48d14d366c7cb7a7319c
luke-meadows <lukemeadowsdev@gmail.com>
2023-11-23 15:16:48 +0000

    pardot tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -32,20 +32,7 @@ function MyApp({ Component, pageProps }) {
                     });
                 `}
       </Script>
-      <Script type="text/javascript">
-        piAId = '1050802'; piCId = ''; piHostname = 'go.pardot.com';
-        {`
-                    (function() {
-                      function async_load(){
-                        var s = document.createElement('script'); s.type = 'text/javascript';
-                        s.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + piHostname + '/pd.js';
-                        var c = document.getElementsByTagName('script')[0]; c.parentNode.insertBefore(s, c);
-                      }
-                      if(window.attachEvent) { window.attachEvent('onload', async_load); }
-                      else { window.addEventListener('load', async_load, false); }
-                    })();
-                `}
-      </Script>
+
       <Header setShowSidebar={setShowSidebar} showSidebar={showSidebar} />
       <AnimatePresence initial={false}>
         {showSidebar && (
