e7855c4a2c1898f629f3710149bb527b83c08d80
luke-meadows <lukemeadowsdev@gmail.com>
2024-01-09 14:35:08 +0000

    sub nav

--- components/global/SubNav.js
+++ components/global/SubNav.js
@@ -10,58 +10,66 @@ export default function SubNav() {
       animate="enter"
       exit="exit"
     >
-      <motion.nav
-        variants={navOptionsVariant}
-        initial="initial"
-        animate="enter"
-        exit="exit"
-      >
-        <Link href="/what-we-do/consultancy" alt="Consultancy Page">
-          <a href="">
-            Consultancy
-            <i className="icon-angle-right" />
-          </a>
-        </Link>
-        <Link href="/what-we-do/integration" alt="Integration Page">
-          <a href="">
-            Integration
-            <i className="icon-angle-right" />
-          </a>
-        </Link>
-        <Link href="/what-we-do/manufacturing" alt="Manufacturing Page">
-          <a href="">
-            Manufacturing
-            <i className="icon-angle-right" />
-          </a>
-        </Link>
-        <Link href="/what-we-do/education" alt="Education Page">
-          <a href="">
-            Education
-            <i className="icon-angle-right" />
-          </a>
-        </Link>
-      </motion.nav>
+      <div className="sub-nav-inner">
+        <motion.nav
+          variants={navOptionsVariant}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <Link href="/what-we-do/consultancy" alt="Consultancy Page">
+            <a href="">
+              Consultancy
+              <i className="icon-angle-right" />
+            </a>
+          </Link>
+          <Link href="/what-we-do/integration" alt="Integration Page">
+            <a href="">
+              Integration
+              <i className="icon-angle-right" />
+            </a>
+          </Link>
+          <Link href="/what-we-do/manufacturing" alt="Manufacturing Page">
+            <a href="">
+              Manufacturing
+              <i className="icon-angle-right" />
+            </a>
+          </Link>
+          <Link href="/what-we-do/education" alt="Education Page">
+            <a href="">
+              Education
+              <i className="icon-angle-right" />
+            </a>
+          </Link>
+        </motion.nav>
+      </div>
     </StyledSubNav>
   );
 }
 
 const StyledSubNav = styled(motion.div)`
-  width: fit-content;
-  height: fit-content;
-  padding-bottom: 2rem;
   z-index: 3;
+  .sub-nav-inner {
+    height: fit-content;
+    padding-bottom: 2rem;
+    margin: 0 auto;
+    max-width: 1610px;
+  }
   nav {
     display: flex;
     width: fit-content;
     flex-direction: column;
+    gap: 0.5rem;
     a {
-      margin: 0 1.2rem 0.2rem 0;
-      width: fit-content;
       display: flex;
       align-items: center;
       color: var(--black);
+      background: var(--yellow2);
       font-weight: 500;
       font-size: 0.8rem;
+      padding: 0.5rem 0.8rem;
+      border-radius: 0.3rem;
+      flex: 1;
       &:hover {
         font-weight: 600;
       }
